bb9718699f37aa4b2690b5271ed5d005faeaf119
1notea <92824484+1notea@users.noreply.github.com>
2023-12-15 17:29:37 +0300

    Add files via upload

---
+++ Zakharov/Views/Journal/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Zakharov.Views.Journal
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
